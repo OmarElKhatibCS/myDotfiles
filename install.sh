3295ee753f4ca3f84ec6a1ad8f02154ac130315a
OmarElKhatibCS <omarelkhatib@mail.com>
2020-06-07 19:35:26 +0300

    remove \n from install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo "[* script is running *]\n"
+echo "[* script is running *]"
 
 echo "[* get last vim version *]"
 sudo apt-get update
@@ -8,11 +8,11 @@ echo "[* Done sucessfully! :) *]"
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-echo "[* install tmux and terminator *]\n"
+echo "[* install tmux and terminator *]"
 sudo apt-get install tmux
 sudo apt-get install terminator
 
-echo "[* install terminal bashes *]\n"
+echo "[* install terminal bashes *]"
 cp ./bashrc ~/.bashrc
 cp ./bash_profile ~/.bash_profile
 cp ./tmux.conf ~/.tmux.conf
