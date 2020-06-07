27d88aa4822d3fc40d2e351cfc3d2b5e6470b864
OmarElKhatibCS <omarelkhatib@mail.com>
2020-06-07 19:31:00 +0300

    add tmux and terminator

--- install.sh
+++ install.sh
@@ -8,9 +8,14 @@ echo "[* Done sucessfully! :) *]"
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+echo "[* install tmux and terminator *]\n"
+sudo apt-get install tmux
+sudo apt-get install terminator
+
 echo "[* install terminal bashes *]\n"
 cp ./bashrc ~/.bashrc
 cp ./bash_profile ~/.bash_profile
+cp ./tmux.conf ~/.tmux.conf
 echo "[* Done successfully! :) *]"
 
 
